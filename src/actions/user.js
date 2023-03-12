a904f78110d0e52032a0d5af204c944380c16e7f
unknown <lerabosenko98@gmail.com>
2023-03-12 23:07:32 +0200

    djgvkd

--- src/actions/user.js
+++ src/actions/user.js
@@ -1,44 +1,80 @@
 import axios from "axios";
-import { setUser } from "../reducers/useReducer";
-// import Login from "./../componets/registration/Login";
+import { setUser } from "./../reducers/useReducer";
 
 export const registration = async (email, password) => {
   try {
-    const res = await axios.post(
-      "http://localhost:5000/api/auth/registration",
+    const response = await axios.post(
+      `http://localhost:5000/api/auth/registration`,
       {
         email,
         password,
       }
     );
-    alert(res.data.message);
+    alert(response.data.message);
   } catch (e) {
     alert(e.response.data.message);
   }
 };
+
 export const login = (email, password) => {
   return async (dispatch) => {
     try {
       const response = await axios.post(
-        "http://localhost:5000/api/auth/login",
+        `http://localhost:5000/api/auth/login`,
         {
           email,
           password,
         }
       );
-      if (
-        response.data &&
-        response.data.user === "object" &&
-        response.data.user
-      ) {
-        dispatch(setUser(response.data.user));
-        console.log(response.data, "error");
-        localStorage.setItem("token", response.data.token);
-      } else {
-        console.log("invalid response data");
-      }
+      dispatch(setUser(response.data.user));
+      localStorage.setItem("token", response.data.token);
     } catch (e) {
-      alert(e);
+      alert(e.response.data.message);
     }
   };
 };
+
+// import axios from "axios";
+// import { setUser } from "../reducers/useReducer";
+// // import Login from "./../componets/registration/Login";
+
+// export const registration = async (email, password) => {
+//   try {
+//     const res = await axios.post(
+//       "http://localhost:5000/api/auth/registration",
+//       {
+//         email,
+//         password,
+//       }
+//     );
+//     alert(res.data.message);
+//   } catch (e) {
+//     alert(e.response.data.message);
+//   }
+// };
+// export const login = (email, password) => {
+//   return async (dispatch) => {
+//     try {
+//       const response = await axios.post(
+//         "http://localhost:5000/api/auth/login",
+//         {
+//           email,
+//           password,
+//         }
+//       );
+//       if (
+//         response.data &&
+//         response.data.user === "object" &&
+//         response.data.user
+//       ) {
+//         dispatch(setUser(response.data.user));
+//         console.log(response.data, "error");
+//         localStorage.setItem("token", response.data.token);
+//       } else {
+//         console.log("invalid response data");
+//       }
+//     } catch (e) {
+//       alert(e);
+//     }
+//   };
+// };
--- src/componets/navbar/NavBar.jsx
+++ src/componets/navbar/NavBar.jsx
@@ -2,23 +2,29 @@ import React from "react";
 import "./navBar.css";
 import logo from "../../assets/img/logo.svg";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const NavBar = () => {
+  const isAuth = useSelector((state) => state.user.isAuth);
   return (
     <div className="navbar">
       <div className="container">
         <img src={logo} alt="" className="navbar-logo" />
         <div className="navbar-header">MERN CLOUD</div>
         <div className="autorization">
-          <div className="navbar-login">
-            <Link to="/login">Войти</Link>
-          </div>
+          {!isAuth && (
+            <div className="navbar-login">
+              <Link to="/login">Войти</Link>
+            </div>
+          )}
 
-          <div className="navbar-registration">
-            <Link to="/registration">Регистрация</Link>
-          </div>
+          {!isAuth && (
+            <div className="navbar-registration">
+              <Link to="/registration">Регистрация</Link>
+            </div>
+          )}
 
-          {/* <div className="navbar-login">Выйти</div> */}
+          {isAuth && <div className="navbar-login">Выйти</div>}
         </div>
       </div>
     </div>
