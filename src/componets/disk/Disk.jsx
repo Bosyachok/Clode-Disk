5256d84cdaa0a613f5d3355ccd8a58faaf193a93
unknown <lerabosenko98@gmail.com>
2023-03-21 13:56:02 +0200

    draganfdrop

--- src/componets/disk/Disk.jsx
+++ src/componets/disk/Disk.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { createDir, getFiles, uploadFile } from "./../../actions/file";
 import "./disk.css";
@@ -11,6 +11,7 @@ const Disk = () => {
   const dispatch = useDispatch();
   const currentDir = useSelector((state) => state.files.currentDir);
   const dirStack = useSelector((state) => state.files.dirStack);
+  const [dragEnter, setDragEnter] = useState(false);
 
   useEffect(() => {
     dispatch(getFiles(currentDir));
@@ -27,8 +28,31 @@ const Disk = () => {
     const files = [...event.target.files];
     files.forEach((file) => dispatch(uploadFile(file, currentDir)));
   }
-  return (
-    <div className="disk">
+  function dragEnterHandler(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    setDragEnter(true);
+  }
+  function dragLeaveHandler(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    setDragEnter(false);
+  }
+  function dropHandler(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    let files = [...event.dataTransfer.files];
+    files.forEach((file) => dispatch(uploadFile(file, currentDir)));
+    setDragEnter(false);
+  }
+
+  return !dragEnter ? (
+    <div
+      className="disk"
+      onDragEnter={dragEnterHandler}
+      onDragLeave={dragLeaveHandler}
+      onDragOver={dragEnterHandler}
+    >
       <div className="disk-btn">
         <button className="disk-back" onClick={() => backHandler()}>
           <img src={back} alt="" />
@@ -51,6 +75,16 @@ const Disk = () => {
       </div>
       <FileList />
       <Popup />
+    </div>
+  ) : (
+    <div
+      className="drop-area"
+      onDragEnter={dragEnterHandler}
+      onDragLeave={dragLeaveHandler}
+      onDragOver={dragEnterHandler}
+      onDrop={dropHandler}
+    >
+      Перетащите файлы сюды
     </div>
   );
 };
